197d0d9c1942a426b56f1807303affce520e32b4
Muhammad Catubig <mbcatubig14@gmail.com>
2021-02-13 11:32:59 +0000

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t mbcatubig14/multi-client-10-14:latest -t mbcatubig14/multi-client-10-14:$SHA ./client
+docker build -t mbcatubig14/multi-server-10-14:latest -t mbcatubig14/multi-server-10-14:$SHA ./server
+docker build -t mbcatubig14/multi-worker-10-14:latest -t mbcatubig14/multi-worker-10-14:$SHA ./worker
+
+docker push mbcatubig14/multi-client-10-14:latest
+docker push mbcatubig14/multi-server-10-14:latest
+docker push mbcatubig14/multi-worker-10-14:latest
+
+docker push mbcatubig14/multi-client-10-14:$SHA
+docker push mbcatubig14/multi-server-10-14:$SHA
+docker push mbcatubig14/multi-worker-10-14:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=mbcatubig14/multi-server-10-14:$SHA
+kubectl set image deployments/client-deployment server=mbcatubig14/multi-client-10-14:$SHA
+kubectl set image deployments/worker-deployment server=mbcatubig14/multi-worker-10-14:$SHA
