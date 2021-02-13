960c8494a13cdf7983152eca685197fa78cd0bb2
Muhammad Catubig <mbcatubig14@gmail.com>
2021-02-13 11:49:51 +0000

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ docker push mbcatubig14/multi-worker-10-14:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=mbcatubig14/multi-server-10-14:$SHA
-kubectl set image deployments/client-deployment server=mbcatubig14/multi-client-10-14:$SHA
-kubectl set image deployments/worker-deployment server=mbcatubig14/multi-worker-10-14:$SHA
+kubectl set image deployments/client-deployment client=mbcatubig14/multi-client-10-14:$SHA
+kubectl set image deployments/worker-deployment worker=mbcatubig14/multi-worker-10-14:$SHA
